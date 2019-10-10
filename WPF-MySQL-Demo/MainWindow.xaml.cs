98a199d09f1154e6b86f3c900490b07b24a31e3c
Koji <20028773@tafe.wa.edu.au>
2019-10-10 14:52:16 +0800

    First Commit

--- WPF-MySQL-Demo/MainWindow.xaml.cs
+++ WPF-MySQL-Demo/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPF_MySQL_Demo
 {
@@ -23,6 +17,82 @@ namespace WPF_MySQL_Demo
         public MainWindow()
         {
             InitializeComponent();
+
+            ScanStatusKeysInBackground();
+
+            FillTable();
+        }
+
+        public void CheckStatusKeys()
+        {
+            NumLockStatus.Foreground = Keyboard.IsKeyToggled(Key.NumLock) ? Brushes.Red : Brushes.Gray;
+            CapsLockStatus.Foreground = Keyboard.IsKeyToggled(Key.CapsLock) ? Brushes.Red : Brushes.Gray;
+            ScrollLockStatus.Foreground = Keyboard.IsKeyToggled(Key.Scroll) ? Brushes.Red : Brushes.Gray;
+        }
+
+        public async Task ScanStatusKeysInBackground()
+        {
+            while (true)
+            {
+                CheckStatusKeys();
+                await Task.Delay(100);
+            }
+        }
+
+        public void FillTable(string searchTerm = "")
+        {
+            string connStr = "server=localhost;" +
+                             "user=nmt_demo_user;" +
+                             "database=nmt_demo;" +
+                             "port=3306;" +
+                             "password=Password1";
+
+            try
+            {
+                MessageTextBlock.Text = "Connecting to MySql ...";
+
+                string sql = "select * from locations";
+
+                if (!string.IsNullOrEmpty(searchTerm))
+                {
+                    sql += " where location_name like '%" + searchTerm.Trim() + "%'";
+                }
+
+                MessageTextBox.Text = sql;
+
+                using (MySqlConnection connection = new MySqlConnection(connStr))
+                {
+                    connection.Open();
+                    MessageTextBlock.Text = "Processing ...";
+
+                    using (MySqlCommand cmdSel = new MySqlCommand(sql, connection))
+                    {
+                        DataTable dt = new DataTable();
+                        MySqlDataAdapter da = new MySqlDataAdapter(cmdSel);
+                        da.Fill(dt);
+                        LocationDataGrid.DataContext = dt;
+                    }
+
+                    connection.Close();
+                }
+                MessageTextBlock.Text = "Ready";
+            }
+            catch (Exception e)
+            {
+                MessageTextBlock.Text = "Error: " + e.Message;
+            }
+        }
+
+        private void FilterTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            FillTable(FilterTextBox.Text);
+        }
+
+        private void BtnAddCountry_Click(object sender, RoutedEventArgs e)
+        {
+            LocationAdd locationAdd = new LocationAdd();
+            locationAdd.ShowDialog();
+            FillTable(FilterTextBox.Text);
         }
     }
 }
